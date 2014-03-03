d78adacffb7c64814c6b119dc69d2b226b282383
Gaël PORT <gael.port@rbs.fr>
2014-03-03 10:22:55 +0100

    Fix routes declaration + minor fixes.

--- Cookbook/Assets/Admin/admin.js
+++ Cookbook/Assets/Admin/admin.js
@@ -1,22 +1,3 @@
 (function () {
 	"use strict";
-
-	var app = angular.module('RbsChange');
-
-	// Register default editors:
-	// Do not declare an editor here if you have an 'editor.js' for your Model.
-	__change.createEditorsForLocalizedModel('Tutorial_Cookbook_Recipe');
-
-	/**
-	 * Routes and URL definitions.
-	 */
-	app.config(['$provide', function ($provide)
-	{
-		$provide.decorator('RbsChange.UrlManager', ['$delegate', function ($delegate)
-		{
-			$delegate.module('Tutorial_Cookbook', 'Tutorial/Cookbook', { 'redirectTo': 'Tutorial/Cookbook/Recipe/'});
-			$delegate.routesForLocalizedModels(['Tutorial_Cookbook_Recipe']);
-			return $delegate.module(null);
-		}]);
-	}]);
 })();
